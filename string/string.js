bfa07e0db02ae61bf7c4f34dfa072ec071bc4b57
ngtan <tannd1993@gmail.com>
2016-05-23 14:11:13 +0700

    ucfirst function

--- string/string.js
+++ string/string.js
@@ -19,3 +19,14 @@ function countWords(str) {
   return str.constructor === ''.constructor &&
          str.replace(/(^\s*)|(\s*$)/gi, '').replace(/\s+/gi, ' ').split(' ').length;
 }
+
+/**
+ * capitalize the first character of a string
+ * @param  {String} str
+ * @return {String}
+ */
+function ucfirst(str) {
+  return str.constructor === ''.constructor &&
+         str.trim() &&
+         str.charAt(0).toUpperCase() + str.slice(1);
+}
