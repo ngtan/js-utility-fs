699e489794fac9fe7ea34f390a93aadc5b15e7ae
ngtan <tannd1993@gmail.com>
2016-05-25 10:33:43 +0700

    factorial function

--- math/math.js
+++ math/math.js
@@ -7,3 +7,16 @@
 function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+/**
+ * Factorial
+ * @param  {Number} num
+ * @return {Number}
+ */
+function factorial(num) {
+  if (0 === num) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+}
