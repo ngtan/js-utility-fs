6bb3b0411da87147e5ac283d6523abf9d98db107
ngtan <tannd1993@gmail.com>
2016-05-30 09:32:45 +0700

    getDaysInMonth function

--- date/date.js
+++ date/date.js
@@ -58,3 +58,35 @@ function getNameInWeek(day) {
     'Saturday'
   ][day];
 }
+
+/**
+ * Get days in month
+ * @param  {Number} year
+ * @param  {Number} month
+ * @return {Number}
+ */
+function getDaysInMonth(year, month) {
+  if (!('number' === typeof month && !isNaN(month))) {
+    return -1;
+  }
+
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      return 31;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    case 2:
+      return getDateInFeb();
+    default:
+      return -1;
+  }
+}
