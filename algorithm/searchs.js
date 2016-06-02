b9751f435c88396a1b3de48f663f0e0f3473920f
ngtan <tannd1993@gmail.com>
2016-06-02 09:32:16 +0700

    binary search function

--- algorithm/searchs.js
+++ algorithm/searchs.js
@@ -13,3 +13,30 @@ function linearSearch(arr, find) {
 
   return -1;
 }
+
+/**
+ * Binary search only works on sorted arrays
+ * @param  {[type]} arr  [description]
+ * @param  {[type]} find [description]
+ * @return {[type]}      [description]
+ */
+function binarySearch(arr, find) {
+  var left = 0,
+      right = arr.length - 1,
+      mid;
+
+  while (left <= right) {
+    mid =  Math.floor((left + right) / 2);
+    if (arr[mid] === find) {
+      return mid;
+    }
+    else if (arr[mid] < find) {
+      left = mid + 1;
+    }
+    else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
